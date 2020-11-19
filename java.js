9d986805a9fd48eb7418c5be089a4cc6511e70bd
Autour Love Jr <autourlovejr@yahoo.com>
2020-11-19 11:24:00 -0500

    more mobile first tweaks

--- java.js
+++ java.js
@@ -1,1 +1,10 @@
-alert("Do You Make over $100,000 a year?");
+let salary = prompt("Do you make at least 100,000 a year?")
+
+let name = prompt("What is Your Name?");
+
+let email = prompt('What is your email?');
+
+let greeting = `<h2>Welcome ${name}, how can we serve you today?</h2>`;
+
+document.querySelector('main');
+
