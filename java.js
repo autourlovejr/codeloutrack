fd0ebeaa9344dcfd47fbf068a882946f81e1fcb2
Autour Love Jr <autourlovejr@yahoo.com>
2020-11-20 11:01:32 -0500

    andrei isnt as cool as i thought

--- java.js
+++ java.js
@@ -2,13 +2,26 @@ let Henry = ['yes'];
 let name = prompt('Hello, What is your name?')
 let search = prompt('Do you make over $100,000 a year?')
 
-// let name = What is your name?;
-
 if (Henry.includes(search.toLowerCase())) {
-    message = `Hello, ${name}, How may we serve you? `;
+    message = `Hello, ${name.toUpperCase()}, How may we serve you? `;
 } else {
-    message =  `This service may not be for you ${name}, sorry!`;
+    message =  `This service may not be for you, ${name.toUpperCase()}, sorry!`;
 }
 
 
+
+
 document.querySelector('main').innerHTML = `<p>${message}</p>`;
+
+let Rando = Math.floor( Math.random() * 6) + 1;
+
+console.log(`Enjoy this random number ${Rando}`);
+
+const h2 = document.querySelector('h2');
+let html = '';
+
+for ( let i = 1; i <= 10; i++ ) {
+    html += `<div>${i}</div>`;
+}
+
+h2.innerHTML = html;
